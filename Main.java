f87ac43daae3671042bf85d242d888df0b6deee4
Randy Kaneer <s11306958@students.duvalschools.org>
2021-10-26 09:31:03 -0400

    Testing the video's code

---
+++ Main.java
@@ -0,0 +1,11 @@
+//Randy Kaneer, APCSA, Topic-2.1, 10/26/2021, 8:33, v0.5
+
+public class Main {
+    private String something;
+    private int number;
+
+    public Main(String smth, int num) {
+        something = smth;
+        number = num;
+    }
+}
