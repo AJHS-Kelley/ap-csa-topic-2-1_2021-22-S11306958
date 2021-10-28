d8245f81a4e6641ef54781820adfbac2d14c3db0
Randy Kaneer <s11306958@students.duvalschools.org>
2021-10-28 09:30:01 -0400

    Defined 3 classes

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-//Randy Kaneer, APCSA, Topic-2.1, 10/26/2021, 8:33, v0.5
+//Randy Kaneer, APCSA, Topic-2.1, 10/28/2021, 9:10, v1.0
 
 public class Main {
     private String something;
@@ -7,5 +7,25 @@ public class Main {
     public Main(String smth, int num) {
         something = smth;
         number = num;
+    }
+}
+
+public class Shoes {
+    private double size;
+    private String material;
+
+    public Shoes(double sz, String mtrl) {
+        size = sz;
+        material = mtrl;
+    }
+}
+
+public class Cereal {
+    private String cerealType;
+    private String milkType;
+
+    public Cereal(String cT, String mK) {
+        cerealType = cT;
+        milkType = mK;
     }
 }
